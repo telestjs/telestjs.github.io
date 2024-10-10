23030778d700f34a63ac328fec042ffbba94d0a2
telestjs <37315484+telestjs@users.noreply.github.com>
2024-10-10 09:11:48 -0500

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Mudar a cor da navbar
+
+function alterarCorNavbar () {
+  const navbar = document.querySelector('nav');
+  const booking = document.querySelector('.booking');
+  const booking
+}
